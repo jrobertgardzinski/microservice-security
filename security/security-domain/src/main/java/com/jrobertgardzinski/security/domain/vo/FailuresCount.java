5fe62e2351927cd22a82595a4c01f301171899c2
Robert Gardziński <jrobertgardzinski@gmail.com>
2025-02-20 00:55:49 +0100

    Unit tests

--- security/security-domain/src/main/java/com/jrobertgardzinski/security/domain/vo/FailuresCount.java
+++ security/security-domain/src/main/java/com/jrobertgardzinski/security/domain/vo/FailuresCount.java
@@ -1,7 +1,7 @@
 package com.jrobertgardzinski.security.domain.vo;
 
 public record FailuresCount(int count) {
-    private static final int LIMIT = 3;
+    public static final int LIMIT = 3;
 
     public boolean hasReachedTheLimit() {
         return count == LIMIT;
--- security/security-domain/src/test/java/com/jrobertgardzinski/security/domain/service/SecurityServiceTest.java
+++ security/security-domain/src/test/java/com/jrobertgardzinski/security/domain/service/SecurityServiceTest.java
@@ -1,25 +1,36 @@
 package com.jrobertgardzinski.security.domain.service;
 
+import com.jrobertgardzinski.security.domain.entity.AuthenticationBlock;
+import com.jrobertgardzinski.security.domain.entity.FailedAuthentication;
+import com.jrobertgardzinski.security.domain.entity.Token;
 import com.jrobertgardzinski.security.domain.entity.User;
+import com.jrobertgardzinski.security.domain.event.authentication.AuthenticationFailedEvent;
+import com.jrobertgardzinski.security.domain.event.authentication.AuthenticationFailuresLimitReachedEvent;
+import com.jrobertgardzinski.security.domain.event.authentication.AuthenticationPassedEvent;
+import com.jrobertgardzinski.security.domain.event.authentication.UserNotFoundEvent;
 import com.jrobertgardzinski.security.domain.event.registration.RegistrationPassedEvent;
 import com.jrobertgardzinski.security.domain.event.registration.UserAlreadyExistsEvent;
 import com.jrobertgardzinski.security.domain.repository.AuthenticationBlockRepository;
 import com.jrobertgardzinski.security.domain.repository.FailedAuthenticationRepository;
 import com.jrobertgardzinski.security.domain.repository.TokenRepository;
 import com.jrobertgardzinski.security.domain.repository.UserRepository;
-import com.jrobertgardzinski.security.domain.vo.UserDetails;
-import com.jrobertgardzinski.security.domain.vo.UserId;
+import com.jrobertgardzinski.security.domain.vo.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SecurityServiceTest {
@@ -31,8 +42,6 @@ class SecurityServiceTest {
     FailedAuthenticationRepository failedAuthenticationRepository;
     @Mock
     AuthenticationBlockRepository authenticationBlockRepository;
-    @Mock
-    UserDetails userDetails;
 
     SecurityService securityService;
 
@@ -42,7 +51,10 @@ class SecurityServiceTest {
     }
 
     @Nested
-    class Registration {
+    class register {
+        @Mock
+        UserDetails userDetails;
+
         @Test
         void positive() {
             when(
@@ -62,5 +74,131 @@ class SecurityServiceTest {
             assertTrue(securityService.register(userDetails).getClass()
                     .isAssignableFrom(UserAlreadyExistsEvent.class));
         }
+    }
+
+    @Nested
+    class authenticate {
+        @Mock
+        Email email;
+        @Mock
+        Password password;
+        @Mock
+        User user;
+
+        @Nested
+        class Positive {
+            @Mock
+            Token token;
+            @Mock
+            TokenDetails tokenDetails;
+
+            @Test
+            void positive() {
+                when(
+                        userRepository.findUserByEmail(email))
+                        .thenReturn(
+                                Optional.of(user));
+                when(
+                        user.enteredRight(password))
+                        .thenReturn(
+                                true);
+                when(
+                        tokenRepository.createAuthorizationToken(user.id()))
+                        .thenReturn(
+                                token);
+                when(
+                        token.details())
+                        .thenReturn(
+                                tokenDetails);
+
+                var result = securityService.authenticate(email, password);
+
+                verify(failedAuthenticationRepository, times(1)).removeAllFor(user.id());
+                verify(authenticationBlockRepository, times(1)).removeAllFor(user.id());
+                verify(tokenRepository, times(1)).createAuthorizationToken(user.id());
+                assertTrue(result.getClass().isAssignableFrom(AuthenticationPassedEvent.class));
+            }
+        }
+
+        @Nested
+        class Negative {
+            @Mock
+            FailedAuthentication failedAuthentication;
+
+            public static Stream<Arguments> source() {
+                return IntStream.range(0, FailuresCount.LIMIT).boxed().map(Arguments::of);
+            }
+
+            @ParameterizedTest
+            @MethodSource("source")
+            void negative(int attempt) {
+                when(
+                        userRepository.findUserByEmail(email))
+                        .thenReturn(
+                                Optional.of(user));
+                when(
+                        failedAuthenticationRepository.countFailuresBy(user.id()))
+                        .thenReturn(
+                                new FailuresCount(attempt));
+                when(
+                        failedAuthenticationRepository.create(any()))
+                        .thenReturn(
+                                failedAuthentication);
+
+
+                var result = securityService.authenticate(email, password);
+
+                verify(failedAuthenticationRepository, times(1)).create(any());
+                assertTrue(result.getClass().isAssignableFrom(AuthenticationFailedEvent.class));
+            }
+
+            @Test
+            void userNotFound() {
+                when(
+                        userRepository.findUserByEmail(email))
+                        .thenReturn(
+                                Optional.empty());
+
+                var result = securityService.authenticate(email, password);
+
+                assertTrue(result.getClass().isAssignableFrom(UserNotFoundEvent.class));
+            }
+
+            @Nested
+            class CausingBlockade {
+                @Mock
+                AuthenticationBlock authenticationBlock;
+                @Mock
+                AuthenticationBlockDetails authenticationBlockDetails;
+
+                @Test
+                void activateBlockade() {
+                    when(
+                            userRepository.findUserByEmail(email))
+                            .thenReturn(
+                                    Optional.of(user));
+                    when(
+                            failedAuthenticationRepository.countFailuresBy(user.id()))
+                            .thenReturn(
+                                    new FailuresCount(FailuresCount.LIMIT));
+                    when(
+                            authenticationBlockRepository.create(any()))
+                            .thenReturn(
+                                    authenticationBlock);
+                    when(
+                            authenticationBlock.details())
+                            .thenReturn(
+                                    authenticationBlockDetails);
+
+                    var result = securityService.authenticate(email, password);
+
+                    verify(failedAuthenticationRepository, times(1)).removeAllFor(user.id());
+                    verify(authenticationBlockRepository, times(1)).create(any());
+                    assertTrue(result.getClass().isAssignableFrom(AuthenticationFailuresLimitReachedEvent.class));
+                }
+            }
+
+        }
+
     }
 }
