f43632c9ce7e6e9168052a8a88bb8dbbefb280c5
Robert Gardziński <jrobertgardzinski@gmail.com>
2025-04-10 22:31:45 +0200

    Add todo

--- security/security-domain/src/main/java/com/jrobertgardzinski/security/domain/aggregate/AuthorizedUserAggregate.java
+++ security/security-domain/src/main/java/com/jrobertgardzinski/security/domain/aggregate/AuthorizedUserAggregate.java
@@ -4,6 +4,7 @@ import com.jrobertgardzinski.security.domain.vo.AuthorizationToken;
 import com.jrobertgardzinski.security.domain.vo.Email;
 import com.jrobertgardzinski.security.domain.vo.RefreshToken;
 
+// TODO - that was close! Rename AuthorizationToken to AccessToken and then follow https://stackoverflow.com/a/54378384/11382755
 public record AuthorizedUserAggregate(
         Email email,
         RefreshToken refreshToken,
