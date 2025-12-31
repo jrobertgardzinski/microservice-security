7cbf358e74eb4f99a3d73b3eb06452cdd7bb73cd
Robert Gardziński <jrobertgardzinski@gmail.com>
2025-12-31 07:48:48 +0100

    cleanup poms and introduce test-starter

--- password-hash-algorithm/password-hash-algorithm-argon2/src/test/java/com/jrobertgardzinski/security/domain/vo/security/service/OptimalNumberOfIterations.java
+++ password-hash-algorithm/password-hash-algorithm-argon2/src/test/java/com/jrobertgardzinski/security/domain/vo/security/service/OptimalNumberOfIterations.java
@@ -3,15 +3,13 @@ package com.jrobertgardzinski.security.domain.vo.security.service;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import de.mkammerer.argon2.Argon2Helper;
-import org.junit.jupiter.api.Test;
 
 import static com.jrobertgardzinski.security.domain.vo.security.service.HashAlgorithmAdapter.MEM_LIMIT;
 import static com.jrobertgardzinski.security.domain.vo.security.service.HashAlgorithmAdapter.PARALLELISM;
 
 public class OptimalNumberOfIterations {
     // https://github.com/phxql/argon2-jvm?tab=readme-ov-file#recommended-parameters
-    @Test
-    void run() {
+    public static void main(String[] args) {
         long REQUIRED_CALCULATION_TIME = 1000;
 
         Argon2 argon2 = Argon2Factory.create();
