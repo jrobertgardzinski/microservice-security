76957a1a54dfa81397149efdd0881da83cdb2858
Robert Gardziński <jrobertgardzinski@gmail.com>
2026-02-08 06:21:11 +0100

    Refactor poms

--- security-domain-model/src/main/java/com/jrobertgardzinski/security/domain/vo/IpAddress.java
+++ security-domain-model/src/main/java/com/jrobertgardzinski/security/domain/vo/IpAddress.java
@@ -1,17 +1,13 @@
 package com.jrobertgardzinski.security.domain.vo;
 
-import org.apache.commons.validator.routines.InetAddressValidator;
-
 import java.util.Objects;
 
 public record IpAddress (String value) {
 
-    private static final InetAddressValidator validator = InetAddressValidator.getInstance();
-    
     public IpAddress {
         Objects.requireNonNull(value);
 
-        if (!validator.isValid(value)) {
+        if (!IpAddressValidator.isValid(value)) {
             throw new IllegalArgumentException(value + " is not valid!");
         }
     }
