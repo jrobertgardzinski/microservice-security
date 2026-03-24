0b7cf9def8ad3648ec3c0f18990ff59257d781e4
Robert Gardziński <jrobertgardzinski@gmail.com>
2026-03-24 19:35:06 +0100

    Add config and system to microservice-security.

---
+++ security-system/src/main/java/com/jrobertgardzinski/security/system/feature/RefreshSession.java
@@ -0,0 +1,45 @@
+package com.jrobertgardzinski.security.system.feature;
+
+import com.jrobertgardzinski.security.config.SessionTokensConfig;
+import com.jrobertgardzinski.security.domain.entity.SessionTokens;
+import com.jrobertgardzinski.security.domain.event.refresh.NoRefreshTokenFoundEvent;
+import com.jrobertgardzinski.security.domain.event.refresh.RefreshTokenEvent;
+import com.jrobertgardzinski.security.domain.event.refresh.RefreshTokenExpiredEvent;
+import com.jrobertgardzinski.security.domain.event.refresh.RefreshTokenPassedEvent;
+import com.jrobertgardzinski.security.domain.repository.AuthorizationDataRepository;
+import com.jrobertgardzinski.security.domain.vo.Email;
+import com.jrobertgardzinski.security.domain.vo.SessionRefreshRequest;
+import com.jrobertgardzinski.security.domain.vo.token.RefreshToken;
+import com.jrobertgardzinski.security.domain.vo.token.expiration.RefreshTokenExpiration;
+
+import java.time.Clock;
+import java.util.function.Function;
+
+public class RefreshSession implements Function<SessionRefreshRequest, RefreshTokenEvent> {
+    private final AuthorizationDataRepository authorizationDataRepository;
+    private final Clock clock;
+    private final SessionTokensConfig config;
+
+    public RefreshSession(AuthorizationDataRepository authorizationDataRepository, Clock clock, SessionTokensConfig config) {
+        this.authorizationDataRepository = authorizationDataRepository;
+        this.clock = clock;
+        this.config = config;
+    }
+
+    @Override
+    public RefreshTokenEvent apply(SessionRefreshRequest sessionRefreshRequest) {
+        Email email = sessionRefreshRequest.email();
+        RefreshToken refreshToken = sessionRefreshRequest.refreshToken();
+        RefreshTokenExpiration refreshTokenExpiration = authorizationDataRepository.findRefreshTokenExpirationBy(email, refreshToken);
+        if (refreshTokenExpiration == null) {
+            return new NoRefreshTokenFoundEvent(email);
+        }
+        authorizationDataRepository.deleteBy(email);
+        if (refreshTokenExpiration.hasExpired(clock)) {
+            return new RefreshTokenExpiredEvent(email);
+        }
+        return new RefreshTokenPassedEvent(
+                authorizationDataRepository.create(
+                        SessionTokens.createFor(email, config.refreshTokenValidityHours().value(), config.accessTokenValidityHours().value(), clock)));
+    }
+}
---
+++ security-system/src/main/java/com/jrobertgardzinski/security/system/usecase/RegisterUseCase.java
@@ -0,0 +1,25 @@
+package com.jrobertgardzinski.security.system.usecase;
+
+import com.jrobertgardzinski.security.system.factory.RegisterFactory;
+import com.jrobertgardzinski.security.system.factory.UserRegistrationValidationException;
+import com.jrobertgardzinski.security.system.feature.Register;
+import com.jrobertgardzinski.security.domain.vo.UserRegistration;
+
+public class RegisterUseCase {
+    private final Register register;
+    private final RegisterFactory registerFactory;
+
+    public RegisterUseCase(Register register, RegisterFactory registerFactory) {
+        this.register = register;
+        this.registerFactory = registerFactory;
+    }
+
+    public RegisterResult execute(String email, String password) {
+        try {
+            UserRegistration userRegistration = registerFactory.create(email, password);
+            return new RegisterResult.Valid(register.apply(userRegistration));
+        } catch (UserRegistrationValidationException e) {
+            return new RegisterResult.Invalid(e);
+        }
+    }
+}
