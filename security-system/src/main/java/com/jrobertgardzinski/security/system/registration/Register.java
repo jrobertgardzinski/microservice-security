540a9d33b4cca981717b40a315140f476a5428a0
jrobertgardzinski <jrobertgardzinski@gmail.com>
2026-06-19 20:47:41 +0200

    Remove Decision, carry the value from supplier in Outcome.

--- security-system/src/main/java/com/jrobertgardzinski/security/system/registration/Register.java
+++ security-system/src/main/java/com/jrobertgardzinski/security/system/registration/Register.java
@@ -7,7 +7,6 @@ import com.jrobertgardzinski.password.domain.PlaintextPassword;
 import com.jrobertgardzinski.password.policy.CreatePasswordHash;
 import com.jrobertgardzinski.security.domain.entity.User;
 import com.jrobertgardzinski.security.domain.repository.UserRepository;
-import com.jrobertgardzinski.util.constraint.Decision;
 import com.jrobertgardzinski.util.constraint.Outcome;
 
 import java.util.List;
@@ -24,48 +23,23 @@ public class Register {
         this.createPasswordHash = createPasswordHash;
     }
 
+    // TODO it surely may be improved! First, check for errors, then operate on outcomes reducing the risk of using suppliers elsewhere than for creating outcomes!
     public RegisterResult execute(Supplier<Email> email, Supplier<PlaintextPassword> password) {
-        Decision<Email> emailDecision = canRegister.evaluate(email);
-        List<String> emailErrors = emailDecision.errorCodes();
+        Outcome<Email> emailOutcome = canRegister.evaluate(email);
+        List<String> emailErrors = emailOutcome.errorCodes();
 
         Outcome<HashedPassword> passwordOutcome = createPasswordHash.create(password);
         List<String> passwordErrors = passwordOutcome.errorCodes();
 
-        return !emailErrors.isEmpty() || !passwordErrors.isEmpty() ?
-                new RegisterResult.Rejected(emailErrors, passwordErrors) :
-                passwordOutcome.findValue()
-                        .map(hash -> {
-                            User userToSave = new User(email.get(), hash);
-                            User savedUser = userRepository.save(userToSave);
-                            return new RegisterResult.Registered(savedUser);
-                        })
-                        .orElseThrow();
-    }
-
-
-/*    public RegisterResult execute(String rawEmail, String rawPassword) {
-        Email email = null;
-        List<String> emailErrors;
-        try {
-            email = Email.of(rawEmail);
-            emailErrors = canRegister.evaluate(email).errorCodes();
-        } catch (IllegalArgumentException e) {
-            emailErrors = List.of(e.getMessage());
-        }
-
-        HashedPassword hash = null;
-        List<String> passwordErrors;
-        try {
-            Outcome<HashedPassword> outcome = createPasswordHash.create(PlaintextPassword.of(rawPassword));
-            passwordErrors = outcome.errorCodes();
-            hash = outcome.findValue().orElse(null);
-        } catch (IllegalArgumentException e) {
-            passwordErrors = List.of(e.getMessage());
-        }
-
         if (!emailErrors.isEmpty() || !passwordErrors.isEmpty()) {
             return new RegisterResult.Rejected(emailErrors, passwordErrors);
         }
-        return new RegisterResult.Registered(userRepository.save(new User(email, hash)));
-    }*/
+
+        Email validatedEmail = emailOutcome.findValue().get();
+        HashedPassword hashedPassword = passwordOutcome.findValue().get();
+
+        User userToSave = new User(validatedEmail, hashedPassword);
+        User savedUser = userRepository.save(userToSave);
+        return new RegisterResult.Registered(savedUser);
+    }
 }
